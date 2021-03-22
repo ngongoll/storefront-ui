16b8f79cb755165525ee11d3f44e60eb76a9d11d
gandrae <hi@georgandrae.de>
2021-03-22 16:46:09 +0000

    - defines basic colors - defines basic font-sizes based on rems - adjustment of heading-lineheight - adds base styles/globals/_base.scss - first button-adjustments

--- packages/vue/src/stories/styleguides/typography.js
+++ packages/vue/src/stories/styleguides/typography.js
@@ -1,9 +1,12 @@
 export const typography = {
   size: {
-    xs: "0.75rem",
+    xs: "0.6875rem",
     sm: "0.875rem",
     base: "1rem",
-    lg: "1.125rem",
+    lg: "1.375rem",
+    xl: "1.563rem",
+    xxl: "2.438rem",
+    xl3: "3.438rem",
   },
   weight: {
     light: 300,
