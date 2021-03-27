c45edd74a24af04751854b2f007c08088051fab5
gandrae <hi@georgandrae.de>
2021-03-27 16:41:34 +0000

    Tweaks Colors & Buttons

--- packages/vue/src/stories/styleguides/colors.js
+++ packages/vue/src/stories/styleguides/colors.js
@@ -6,12 +6,12 @@ export const colors = {
   info: ["#0468DB", "#e1f4fe"],
   success: ["#5ece7b", "#9ee2b0"],
   warning: ["#ecc713", "#f6e389"],
-  danger: ["#d12727", "#fcede8"],
+  danger: ["#FF3363", "#FAD3DC"],
 };
 
 export const iconColors = {
-  greenPrimary: "#5ece7b",
-  greenSecondary: "#9ee2b0",
+  greenPrimary: "#00F0C2",
+  greenSecondary: "#8DFFE9",
   darkPrimary: "#111111",
   darkSecondary: "#403F3F",
   grayPrimary: "#CCCCCC",
@@ -19,8 +19,8 @@ export const iconColors = {
   grayAccent: "#222222",
   lightPrimary: "#F5F5F5",
   lightSecondary: "#ffffff",
-  redPrimary: "#d12727",
-  redSecondary: "#fcede8",
+  redPrimary: "#FF3363",
+  redSecondary: "#FAD3DC",
   yellowPrimary: "#ecc713",
   yellowSecondary: "#f6e389",
   bluePrimary: "#0468DB",
